311fdd77526a23a0be48c15169f72338fc6c367c
Titanium Trojans <whitmore.robot.8492@gmail.com>
2025-12-04 19:45:28 -0500

    update corner boom md

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/BlueFarCornerCycle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/BlueFarCornerCycle.java
@@ -130,7 +130,7 @@ public class BlueFarCornerCycle extends OpMode {
                 break;
             case _25_StopLaunch:
                 if (runtime.milliseconds() >=1500)     {
-                    robot.driveTrain.CmdDrive(15,-58,0.35,0);
+                    robot.driveTrain.CmdDrive(4,-58,0.35,-180);
                     robot.launcherBlocker.cmdBlock();
                     robot.launcher.cmdStop();
                     runtime.reset();
@@ -140,7 +140,7 @@ public class BlueFarCornerCycle extends OpMode {
                 break;
             case _30_MoveToWall:
                 if (runtime.milliseconds() >= 0)     {
-                    robot.driveTrain.cmdTurn(-58,0.35);
+                    robot.driveTrain.cmdTurn(-65,0.35);
                     robot.transitionRoller.cmdSpin();
                     robot.intake.cmdFoward();
                     currentStage = stage._40_GoForward;
