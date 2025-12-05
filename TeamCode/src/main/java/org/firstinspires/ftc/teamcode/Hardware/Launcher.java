8e750ffdd6480388b5575f78adc12b184019c713
Titanium Trojans <whitmore.robot.8492@gmail.com>
2025-12-04 19:57:01 -0500

    farlaunch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Launcher.java
@@ -62,7 +62,7 @@ public class Launcher extends BaseHardware{
     public static double LkD = 0.0;
     public static double topMotorRPMnear = 2750;
     public static double bottomMotornear = 3350;
-    public static double topMotorRPMfar = 2900;
+    public static double topMotorRPMfar = 2850;
     public static double bottomMotorfar = 4650; //was4600
     public static double topMotorRPMtouch = 2350; //was 2300
     public static double bottomMotortouch = 4000; //was 3800
