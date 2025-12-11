00fd0c9af52d0a0b2a695217d0febcf9e7ba8369
Titanium Trojans <whitmore.robot.8492@gmail.com>
2025-12-10 20:20:58 -0500

    rpm launcher update md

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Launcher.java
@@ -61,8 +61,8 @@ public class Launcher extends BaseHardware{
     public static double LkP = 0.000155;  //was 0.00015
     public static double LkI = 0.0;
     public static double LkD = 0.0;
-    public static double topMotorRPMnear = 2750;
-    public static double bottomMotornear = 3350;
+    public static double topMotorRPMnear = 2900;
+    public static double bottomMotornear = 3600;
     public static double topMotorRPMfar = 2850;
     public static double bottomMotorfar = 4650; //was4600
     public static double topMotorRPMtouch = 2300; //was 2300
